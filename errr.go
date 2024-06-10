c59935b978c03215469253446833b2d5d16a1410
Tyler Boyd <ty@tyboyd.dev>
2024-06-10 10:37:38 -0500

    updated docs

--- errr.go
+++ errr.go
@@ -3,14 +3,14 @@ package errr
 // we use the charm.sh log library
 import "github.com/charmbracelet/log"
 
-// Fatal takes in the potential error, and handles the error with a fatal if it exists
+// Fatal takes in the potential error, and handles the error with a fatal if it exists.
 func Fatal(error error) {
 	if error != nil {
 		log.Fatal(error.Error())
 	}
 }
 
-// Warn takes in the potential error, and handles the error with a warn if it exists
+// Warn takes in the potential error, and handles the error with a warn if it exists.
 func Warn(error error) {
 	if error != nil {
 		log.Warn(error.Error())
